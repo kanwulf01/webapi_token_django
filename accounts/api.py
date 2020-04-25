4562ed05b7639214fa6a230033393b9f62d0ccf2
kanwulf01 <radamanthyzdark@hotmail.com>
2020-04-25 17:35:53 -0500

    test 2

--- accounts/api.py
+++ accounts/api.py
@@ -3,15 +3,7 @@ from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import UserSerializer, UserHerenciaSerializer, LoginSerializer
 
-''' Register API
-Esta clase hereda de genercis, generics APIView, se llama a la clase UserHerenciaSerializer, que es la clase 
-encargada de devolver el usuario con todos sus campos, y de enlazar a usuario con el serializer de la clase medico creada
-models, se define un metodo post, que recibe un request, y args y kargs, se llama al serializer que se instancio en 
-serializer_class con el get_serializer, y se le pasan los datos llegados en formaton json por request.data
-se valida que el serializador con los datos sea valido, se guarda en la base de datos , y se hace una respuesta
-devolviendo al usuario que se creo inmediatamente, y se crea un token pasandole como parametro el usuario que se acabo de crear
-tambien se retorna el token
-'''
+
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = UserHerenciaSerializer
 
@@ -24,13 +16,7 @@ class RegisterAPI(generics.GenericAPIView):
             "token":AuthToken.objects.create(user)[1]
         })
 
-'''
-    loginAPI
 
-se instancia un serializer Loginserializer, que contiene la validacion de los campos llegados por el request
-se usa un metodo en serializer llamado autheticate
-si la informacion llevada al serializador es valida, retorna al usuario y su token*(refresca el token lo crea de nuevo)
-'''
 
 class LoginAPI(generics.GenericAPIView):
     serializer_class = LoginSerializer
@@ -44,10 +30,7 @@ class LoginAPI(generics.GenericAPIView):
             "token":AuthToken.objects.create(user)[1]
         })
 
-'''
-    UserAPi
-valida si tiene permisos de usuario usando el token, si el token existe retorna todos los datos del usuario
-'''
+
 class UserAPI(generics.RetrieveAPIView):
     permission_classes = [
         permissions.IsAuthenticated,
